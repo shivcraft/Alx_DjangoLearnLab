eb045be5da3a74b186fed89d9164bdf2f33a588f
shivcraft <warrenshivairo@gmail.com>
2025-09-15 00:20:48 +0300

    Update admin.py

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,1 +1,13 @@
+# LibraryProject/bookshelf/admin.py
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser  # Make sure this is your custom user model
+
+class CustomUserAdmin(UserAdmin):
+    # Add any customizations here if needed
+    model = CustomUser
+    list_display = ['username', 'email', 'first_name', 'last_name', 'is_staff']
+
+# Register the custom user model with the custom admin
+admin.site.register(CustomUser, CustomUserAdmin)
 
