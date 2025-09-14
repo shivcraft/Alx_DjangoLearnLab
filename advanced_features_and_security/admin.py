fbbe134280ef36ee6e9d52f602cd0a2d6bd585a2
shivcraft <warrenshivairo@gmail.com>
2025-09-14 22:22:09 +0300

    Update admin.py

--- advanced_features_and_security/admin.py
+++ advanced_features_and_security/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser, Article
 
-
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
     list_display = ("email", "first_name", "last_name", "is_staff", "is_active")
@@ -11,17 +10,16 @@ class CustomUserAdmin(UserAdmin):
         (None, {"fields": ("email", "password")}),
         ("Personal Info", {"fields": ("first_name", "last_name")}),
         ("Permissions", {"fields": ("is_staff", "is_active", "is_superuser", "groups", "user_permissions")}),
-        ("Important dates", {"fields": ("last_login", "date_joined")}),
+        ("Important dates", {"fields": ("last_login",)}),
     )
     add_fieldsets = (
         (None, {
             "classes": ("wide",),
-            "fields": ("email", "password1", "password2", "is_staff", "is_active")}
-        ),
+            "fields": ("email", "first_name", "last_name", "password1", "password2", "is_staff", "is_active"),
+        }),
     )
     search_fields = ("email",)
     ordering = ("email",)
-
 
 admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Article)
